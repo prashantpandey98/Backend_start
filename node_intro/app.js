f3c253632edf4103458542c686128a97b0ff20b7
Prashant Pandey <yofalcon98@gmail.com>
2023-10-17 12:31:55 +0530

    express paths, body-parser and app.get()/post()

--- node_intro/app.js
+++ node_intro/app.js
@@ -1,15 +1,23 @@
-const http = require("http");
-
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("This is a middleware");
-  next();
-});
-app.use((req, res, next) => {
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/add-product", (req, res, next) => {
   console.log("This is a another middleware");
-  res.send("{ key1: value }");
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="product"><input type="number" name="size"><button type="submit">Add Product</button></form>'
+  );
+});
+
+app.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
+  res.send("<h1>Hello Express</h1>");
 });
 
 app.listen(3000);
